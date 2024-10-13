6db59ee8c6d41fea39972ed093b2bd0191c88cf3
esat_arslan <arslanesat515@gmail.com>
2024-10-13 19:58:51 +0300

    CRUD backend

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -3,26 +3,41 @@ using Microsoft.AspNetCore.Mvc;
 using Persistence;
 using Microsoft.EntityFrameworkCore; 
 using Domain;
+using MediatR;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context) 
-        {
-            _context = context;
-        }
-
-        [HttpGet]
+        [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());   
         }
-        [HttpGet("{id}")]
+        [HttpGet("{id}")] //api/activities/928147usa7d6a87
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
-        }  
+            return await Mediator.Send(new Details.Query{Id=id});
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command { Activity = activity });
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            await Mediator.Send(new Delete.Command{Id=id});
+            return Ok();
+        }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,13 @@
+using API.Extensions;   
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite("Data Source=/home/esata/Projects/reactt/Reactivities/API/reactivities.db");});
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy =>{
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
