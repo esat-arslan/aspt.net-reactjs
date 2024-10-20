a8f30adcf390105441c5b12105d607861bf59c26
esat_arslan <arslanesat515@gmail.com>
2024-10-20 11:40:42 +0300

    Error handling

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,7 +1,7 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
-using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore;
 using Domain;
 using MediatR;
 using Application.Activities;
@@ -11,33 +11,31 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
         [HttpGet] //api/activities
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<IActionResult> GetActivities()
         {
-            return await Mediator.Send(new List.Query());   
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
         [HttpGet("{id}")] //api/activities/928147usa7d6a87
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        public async Task<IActionResult> GetActivity(Guid id)
         {
-            return await Mediator.Send(new Details.Query{Id=id});
+            var result = await Mediator.Send(new Details.Query { Id = id });
+            return HandleResult(result);
         }
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            await Mediator.Send(new Create.Command { Activity = activity });
-            return Ok();
+            return HandleResult(await Mediator.Send(new Create.Command{Activity = activity}));
         }
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
-            await Mediator.Send(new Edit.Command { Activity = activity });
-            return Ok();
+            return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
-            await Mediator.Send(new Delete.Command{Id=id});
-            return Ok();
+            return HandleResult(await Mediator.Send(new Delete.Command{Id=id}));
         }
     }
 }
--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,5 +1,7 @@
+using Application.Core;
 using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -7,11 +9,11 @@ namespace Application.Activities
 {
     public class Edit
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity{ get; set; }
         }
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -20,11 +22,21 @@ namespace Application.Activities
                 _mapper = mapper;
                 _context = context;
             }
-            public async Task Handle(Command request, CancellationToken cancellationToken)
+                    public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
+                if (activity == null) return null;
                 _mapper.Map(request.Activity, activity);
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync()>0;
+                if(!result) return Result<Unit>.Failure("Failed to update act");
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
